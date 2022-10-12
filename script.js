3d1d15c70949fc0799f91ec5a42d3e50d839b39f
1-aziat-1 <as.ac2727@mail.ru>
2022-10-13 00:15:03 +0300

    task

--- script.js
+++ script.js
@@ -1,2 +1,7 @@
-
+function palindrome(string) {
+  let strin = string.toLowerCase().replace(/[.,\/#!\ \$%\^&\*;:{}'=\-_`~()]/g,"");
+  let arr = strin.split('').reverse().join("");
+  return strin === arr;
+}
+console.log(palindrome("Abba Zabba, you're my only friend"));
 
