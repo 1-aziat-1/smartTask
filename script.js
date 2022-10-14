0c701cbe9d6522c3f1f29ad721c2b26abcdf771d
1-aziat-1 <as.ac2727@mail.ru>
2022-10-15 01:32:09 +0300

    task ready

--- script.js
+++ script.js
@@ -1,12 +1,40 @@
-var uniqueInOrder=function(iterable){
-  let result = [];
-  let n;
-  for(i=0; i<=iterable.length-1; i++){
-    if(iterable[i] !== n){
-      result.push(n=iterable[i]);
+function solution (roman) {
+  let conv = {
+    "M" : 1000, 
+    "CM" : 900, 
+    "D" :500 ,
+    "CD" : 400, 
+    "C" :100 ,
+    "XC" : 90 ,
+    "L" :50 ,
+    "XL" : 40 ,
+    "X" :10 ,
+    "IX" : 9 ,
+    "V" :5 ,
+    "IV" : 4 ,
+    "I" :1,
+  };
+  let arr = roman.split('');
+  let result = 0;
+  let current;
+  let currentValue;
+  let next;
+  let nextValue;
+
+  for(let i = 0; i<arr.length; i++){
+    current = arr[i];
+    currentValue = conv[current];
+
+    next = arr[i+1];
+    nextValue = conv[next];
+
+    if(currentValue<nextValue){
+      result -= currentValue;
+    }else{
+      result += currentValue;
     }
   }
   return result;
 };
 
-console.log(uniqueInOrder('AAAABBBCCDAABBB'));
+console.log(solution('MMMCMXCIII'));
